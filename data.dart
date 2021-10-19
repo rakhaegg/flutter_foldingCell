1ac0850013cad1873fb19c9b36f546acbeae62aa
rakhaegg <43262815+rakhaegg@users.noreply.github.com>
2021-10-19 16:52:47 +0700

    Create data.dart

---
+++ data.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter_application_1/model/ice_cream.dart';
+
+List<IceCream> iceCream = [
+  IceCream(name: 'Stawberry', image: "graphic/ice-cream 01.jpg"),
+  IceCream(name: 'Chocolate', image: "graphic/ice-cream 05.jpg"),
+  IceCream(name: 'Vanilla', image: "graphic/ice-cream 06.jpg"),
+  
+];
